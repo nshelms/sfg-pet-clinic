3583c4dff7126e815e7ed43f269b62abca30a4a4
Nick Helmstetter <nhelmstetter@outlook.com>
2022-04-15 22:23:41 -0400

    Added POJOs. Closes #2.

---
+++ src/main/java/com/sashassitters/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.sashassitters.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/sashassitters/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.sashassitters.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Person owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
