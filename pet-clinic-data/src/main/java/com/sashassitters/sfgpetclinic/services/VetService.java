3696251df0fc31f8869feff6dd40c79d767141a9
Nick Helmstetter <nhelmstetter@outlook.com>
2022-06-25 23:02:09 -0400

    Refactored services to use common base interface. Closes #10

--- pet-clinic-data/src/main/java/com/sashassitters/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/sashassitters/sfgpetclinic/services/VetService.java
@@ -2,12 +2,7 @@ package com.sashassitters.sfgpetclinic.services;
 
 import com.sashassitters.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
     Vet findByLastName(String lastName);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
